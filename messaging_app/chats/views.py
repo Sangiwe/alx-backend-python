65b7009a5c1fa20c4981a734eb7206510892911a
Sangiwe <nokubongasangiwe@gmail.com>
2025-07-27 10:16:13 +0200

    changes

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -2,11 +2,15 @@ from django.shortcuts import render
 from rest_framework import viewsets, permissions
 from .models import Conversation, Message
 from .serializers import ConversationSerializer, MessageSerializer
+from rest_framework import viewsets, status, filters
+from django.contrib.auth import get_user_model
 
 class ConversationViewSet(viewsets.ModelViewSet):
     queryset = Conversation.objects.all()
     serializer_class = ConversationSerializer
     permission_classes = [permissions.IsAuthenticated]  # Optional if auth is used
+    filter_backends = [filters.SearchFilter]  # ✅ using filters
+    search_fields = ['participants__email']
 
     def perform_create(self, serializer):
         # When creating a conversation, add the current user automatically
@@ -19,6 +23,8 @@ class MessageViewSet(viewsets.ModelViewSet):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
     permission_classes = [permissions.IsAuthenticated]
+    filter_backends = [filters.OrderingFilter]  # ✅ also using filters
+    ordering_fields = ['sent_at']
 
     def perform_create(self, serializer):
         # Automatically set the sender to the current user
