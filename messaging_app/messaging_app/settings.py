5dbfe8bbb0a0acbea108d19fef540bed37fba4c3
Sangiwe <nokubongasangiwe@gmail.com>
2025-08-03 19:24:45 +0200

    fixed authentication class

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -132,10 +132,9 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',
-    ],
-        'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ),
+    ],
 }
+
 
 AUTH_USER_MODEL = 'chats.User'
